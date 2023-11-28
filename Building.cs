376b8a3cd98d6fcaab57172aae3dab7f7e2597c1
Linards <linardspakalns@gmail.com>
2023-11-28 18:54:29 +0100

    Commented out Building Functions NPC's and Map

--- Building.cs
+++ Building.cs
@@ -3,12 +3,16 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace WorldOfZuul
 {
+        // Define a Building class
     public class Building
     {
+        // Properties of a building
         public string name;
         public char symbol;
         public int number;
         public List<int> resources;
+
+        // Constructor for the Building class
         public Building (string name, char symbol, int number, List<int> resources)
         {
             this.name = name;
@@ -18,10 +22,13 @@ namespace WorldOfZuul
         }
     }
     
+    // Define a House class that inherits from Building
     public class House : Building
     {
         int humanCount;
         public int survivabilityIndex; //The chances of user dying are 1/survivabilityIndex
+
+        // Constructor for the House class
         public House(string name, int number, List<int> resources, int humanCount, int survivabilityIndex) : base(name, 'l', number, resources)
         {
             this.humanCount = humanCount;
@@ -29,6 +36,7 @@ namespace WorldOfZuul
         }
     }
 
+    // Define an Industrial class that inherits from Building
     public class Industrial : Building
     {
         int range;
@@ -36,6 +44,7 @@ namespace WorldOfZuul
         List<int>? coordinates;
         //int workingPeople;
 
+        // Constructor for the Industrial class
         public Industrial(string name, char symbol, int number, List<int> resources, int range, int impact, List<int>? coordinates/*, int workingPeople|*/) : base(name, symbol, number, resources)
         {
             this.range = range;
@@ -43,19 +52,31 @@ namespace WorldOfZuul
             this.coordinates = coordinates;
             // this.workingPeople = workingPeople; 
         }
+
+        // Method to find houses in the specified range on the map
         public List<Square> FindHousesInRange(Map map)
         {
             List<Square> houseSquares = new();
 
+            // Iterate over possible rows in the range
             for (int possible_row = -this.range; possible_row<=this.range; possible_row++)
             {
+                // Iterate over possible columns in the rang
                 for (int possible_col= - this.range; possible_col<=this.range; possible_col++)
                 {
+                    // Calculate the current coordinates based on the range
                     List<int> curr_coord = new List<int>{this.coordinates[1] + possible_row, this.coordinates[0] + possible_col};
+
+                    // Check if the current coordinates are not the center (0, 0)
+                    // and are within the bounds of the map
                     if ((possible_row != 0 || possible_col != 0) && curr_coord[0] >= 0 && curr_coord[1] >= 0 && curr_coord[0] <= map.this_map[0].Count && curr_coord[1] <= map.this_map.Count)
                     {
+                        // Access the square at the current coordinates
                         Square curr_square = map.this_map[curr_coord[0]][curr_coord[1]];
+
+                        // Check if the square contains a house ('l')
                         if(curr_square.value == 'l')
+                            // Add the square to the list of house squares
                             houseSquares.Add(curr_square);
                     }
                 }
@@ -64,6 +85,7 @@ namespace WorldOfZuul
             return houseSquares;
         }
 
+    // Method to impact buildings in the specified range on the map
     public void ImpactBuildings (Map map)
         {
             foreach (Square house in FindHousesInRange(map))
--- NPCs.cs
+++ NPCs.cs
@@ -1,24 +1,32 @@
 namespace WorldOfZuul
 {
+    // Represents a Non-Player Character (NPC) in the game
     public class NPC
     {
+        // Name of the NPC
         string name;
+
+        // Dictionary to store prompts associated with keys
         Dictionary<string, string> prompts;
         
+        // Constructor to initialize an NPC with a name and prompts
         public NPC(string name, Dictionary<string, string> prompts)
         {
             this.name = name;
             this.prompts = prompts;
         }
         
+        // Get a prompt associated with a specific key
         public string GetPrompt(string key)
         {
+            // Check if the key exists in the prompts dictionary
             if (prompts.TryGetValue(key, out string? value))
             {
                 return value;
             }
             else
             {
+                // Throw an exception if the key is not found
                 throw new ArgumentException($"Key- '{key}' -not found.");
             }
         }
